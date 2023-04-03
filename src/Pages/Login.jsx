d77e6d679d70d2db77e62444a1d09e8fe8c7caf0
Vinícius Coelho <viniciusraposo@gmail.com>
2023-04-03 18:49:01 -0300

    req-4: validação dos inputs

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,18 +1,37 @@
 import { useState } from 'react';
 
 function Login() {
-  // const [user, setUser] = useState({
-  //   email: '',
-  //   senha: '',
-  // });
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+  });
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleValidation = () => {
+    const { email, password } = user;
+    const emailValidation = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(email);
+    const minLength = 6;
+    const passwordValidation = password.length < minLength;
+
+    setIsDisabled(!emailValidation || passwordValidation);
+  };
 
   return (
-    <form>
+    <form onSubmit={ (e) => handleSubmit(e) }>
       <label htmlFor="email-input">
         Email:
         <input
           type="email"
+          value={ user.email }
           id="email-input"
+          onChange={ ({ target: { value } }) => {
+            setUser({ ...user, email: value });
+            handleValidation();
+          } }
           data-testid="email-input"
         />
       </label>
@@ -20,11 +39,23 @@ function Login() {
         Senha:
         <input
           type="password"
+          value={ user.password }
           id="password-input"
+          onChange={ ({ target: { value } }) => {
+            setUser({ ...user, password: value });
+            handleValidation();
+          } }
           data-testid="password-input"
         />
       </label>
-      <button type="submit" data-testid="login-submit-btn">Entrar</button>
+      <button
+        type="submit"
+        data-testid="login-submit-btn"
+        disabled={ isDisabled }
+      >
+        Entrar
+
+      </button>
     </form>
   );
 }
