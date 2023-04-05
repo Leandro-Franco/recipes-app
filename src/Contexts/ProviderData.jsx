20e894690bc60adafdf183648d9939a92425070a
guiDMoura <lucas14pm@gmail.com>
2023-04-05 16:34:42 -0300

    Requisito 14, pequenas alterações para adaptação.

--- src/Contexts/ProviderData.jsx
+++ src/Contexts/ProviderData.jsx
@@ -1,11 +1,5 @@
-import { useState, useEffect, createContext, useMemo, useCallback } from 'react';
+import { useState, createContext, useMemo } from 'react';
 import PropTypes from 'prop-types';
-import {
-  mealsCategories,
-  searchMealsIngredients,
-  mealsNationalities,
-  searchDrinkId,
-  searchMealId } from '../Services/ApiRequest';
 
 const context = createContext();
 
@@ -13,12 +7,6 @@ export default context;
 
 export function Provider({ children }) {
   // const [user, setUser] = useState({});
-  const [dataMeals, setDataMeals] = useState({
-    categories: null,
-    nationalities: null,
-    ingredients: null,
-  });
-  // const [dataDrinks, setDataDrinks] = useState(null);
 
   const [dataSearch, setDataSearch] = useState({
     textSearch: '',
@@ -27,42 +15,12 @@ export function Provider({ children }) {
 
   const [resultsOfSearch, setResultsOfSearch] = useState([]);
 
-  const [detailRecipes, setDetailRecipes] = useState({ detail: null });
-
-  const requestMeals = async () => {
-    const categories = await mealsCategories();
-    const nationalities = await mealsNationalities();
-    const ingredients = await searchMealsIngredients();
-
-    if (ingredients) {
-      setDataMeals({ categories, nationalities, ingredients });
-    }
-  };
-
-  const fetchRecipeDetails = useCallback(async (id, type) => {
-    const food = await searchMealId(id);
-    const drinks = await searchDrinkId(id);
-    const results = type === 'drinks' ? drinks : food;
-    const detail = results.drinks ? results.drinks[0] : results.meals[0];
-
-    if (detail) {
-      setDetailRecipes({ detail });
-    }
-  }, []);
-
-  useEffect(() => {
-    requestMeals();
-  }, []);
-
   const data = useMemo(() => ({
-    dataMeals,
     dataSearch,
     resultsOfSearch,
-    detailRecipes,
-    fetchRecipeDetails,
     setDataSearch,
     setResultsOfSearch,
-  }), [dataMeals, dataSearch, resultsOfSearch, detailRecipes, fetchRecipeDetails]);
+  }), [dataSearch, resultsOfSearch]);
 
   return (
     <context.Provider
